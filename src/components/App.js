69cab8c2531ff4d63d6270eb2fb040ed8778c7d9
cristian.vargas <cristian.vargas@tars.dev>
2019-10-25 16:50:49 -0500

    Redux primeras clases

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Menu from './Menu';
+import Usuarios from './Usuarios'
+
+const Tareas = ()=> (
+  <div>Tareas</div>
+)
+const App = ()=>(
+  <BrowserRouter>
+    <Menu/>
+    <Route exact path='/' component={Usuarios} />
+    <Route exact path='/tareas' component={Tareas} />
+  </BrowserRouter>
+)
+
+export default App;
